75f5b097b96d9972bd05becc9061b56d42fa1a84
Kennedy Ratemo <62438195+ratsken@users.noreply.github.com>
2021-01-07 10:20:40 +0300

    git

--- ereja/settings.py
+++ ereja/settings.py
@@ -20,17 +20,14 @@ from oscar.defaults import OSCAR_DASHBOARD_NAVIGATION
 BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-from dotenv import load_dotenv
-load_dotenv()
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = '^7fcqrcc++559x3o_x=nhr-zqtd3i9viju5!41htbah&)$r!ok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -87,7 +84,7 @@ INSTALLED_APPS = [
 
     'oscar.config.Shop',
     'oscar.apps.analytics.apps.AnalyticsConfig',
-    'apps.checkout.apps.CheckoutConfig',
+    'oscar.apps.checkout.apps.CheckoutConfig',
     'oscar.apps.address.apps.AddressConfig',
     'oscar.apps.shipping.apps.ShippingConfig',
     'oscar.apps.catalogue.apps.CatalogueConfig',
@@ -95,7 +92,7 @@ INSTALLED_APPS = [
     'oscar.apps.communication.apps.CommunicationConfig',
     'oscar.apps.partner.apps.PartnerConfig',
     'oscar.apps.basket.apps.BasketConfig',
-    'apps.payment.apps.PaymentConfig',
+    'oscar.apps.payment.apps.PaymentConfig',
     'oscar.apps.offer.apps.OfferConfig',
     'oscar.apps.order.apps.OrderConfig',
     'oscar.apps.customer.apps.CustomerConfig',
@@ -124,8 +121,6 @@ INSTALLED_APPS = [
     'django_tables2',
     'oscar_invoices',
     'django_social_share',
-    'contactform',
-
 ]
 
 SITE_ID = 1
@@ -245,7 +240,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/public_html/staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
--- ereja/wsgi.py
+++ ereja/wsgi.py
@@ -7,18 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+import sys
 import os
-import dotenv
+
+# Add this line
+sys.path.insert(0, os.path.dirname(__file__))
 
 from django.core.wsgi import get_wsgi_application
 
-dotenv.load_dotenv(
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-)
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ereja.settings')
 
-if os.getenv('DJANGO_SETTINGS_MODULE'):
- os.environ['DJANGO_SETTINGS_MODULE'] = os.getenv('DJANGO_SETTINGS_MODULE')
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
